c83b99e6183e65ca7dbf95c523e7f3645c31fa92
DevFreshman <hoangducmanh2004789@gmail.com>
2026-03-18 02:46:44 +0700

    refactor Ex

--- src/main/java/com/github/hoangducmanh/smart_task_management/application/auth/exception/EmailMismatchException.java
+++ src/main/java/com/github/hoangducmanh/smart_task_management/application/auth/exception/EmailMismatchException.java
@@ -2,11 +2,11 @@ package com.github.hoangducmanh.smart_task_management.application.auth.exception
 
 public class EmailMismatchException extends AuthException {
     public EmailMismatchException(String message) {
-        super(message);
+        super(message, ErrorCode.EMAIL_MISMATCH);
     }
 
     public EmailMismatchException(String message, Throwable cause) {
-        super(message, cause);
+        super(message, ErrorCode.EMAIL_MISMATCH, cause);
     }
 
 }
